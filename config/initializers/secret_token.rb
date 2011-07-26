333c927fb735f73979ebff935c56b3a1625a9cf6
pgturd2 <powerdump556@hotmail.com>
2011-07-25 21:56:41 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAppLaptop::Application.config.secret_token = 'f0f596ac7cfcbcf52e84aec514d913d2165bfc2597787ec05263371ae54f6b17d7b9bdac27c0bafb7b4c86abc6d8e2205f36ae0dd86bf6491eaa61753122228f'
